4585ce9b7798c11342b51d808fbe079f0bbec5c6
fco-barraza87 <advanceplus.coach@gmail.com>
2025-11-10 19:01:46 +0100

    test2
    test2

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').addEventListener('click', () => {
+  alert('Aquí irá la conexión con Supabase Auth.');
+});
+
+document.getElementById('register-btn').addEventListener('click', () => {
+  alert('Aquí se conectará el registro de usuario.');
+});
