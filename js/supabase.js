03949052977f89952c3a5a3a71a8a46af6d77903
fco-barraza87 <advanceplus.coach@gmail.com>
2025-11-27 17:20:37 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -5,7 +5,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://lmlfvbzukymtkcyfromr.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_hmEwUbwWxBFEKl73hIcO8w_zWEFw2TW";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtbGZ2Ynp1a3ltdGtjeWZyb21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjA3MzksImV4cCI6MjA3ODUzNjczOX0.VtetQ9jdaG6ZIDNGKsaTqxllET0nBZBrYDmZgfEdpLU";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
