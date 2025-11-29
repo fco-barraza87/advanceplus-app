781f96c13f86fda9a215bf2f299dbf6e6e1887d8
fco-barraza87 <advanceplus.coach@gmail.com>
2025-11-29 18:19:05 +0100

    107

--- components/user-header.js
+++ components/user-header.js
@@ -26,12 +26,35 @@ export async function loadUserHeader() {
   document.getElementById("headerUserRole").textContent =
     profile?.role?.toUpperCase() || "USER";
 
-  document.getElementById("headerAvatar").src =
-    profile?.avatar_url || "/img/default-avatar.png";
+    const avatarDiv = document.getElementById("headerAvatar");
+
+    if (profile?.avatar_url) {
+    avatarDiv.innerHTML = `<img src="${profile.avatar_url}" alt="avatar">`;
+    } else {
+    const { initials, bg } = generateInitialAvatar(profile?.full_name || "U S");
+    avatarDiv.style.background = bg;
+    avatarDiv.textContent = initials;
+    }
+
 
   // 4) Botón logout
   document.getElementById("btnLogout").addEventListener("click", async () => {
     await supabase.auth.signOut();
     window.location.href = "/index.html";
   });
+
+    function generateInitialAvatar(name) {
+    const initials = name
+        .split(" ")
+        .map(w => w.charAt(0))
+        .join("")
+        .substring(0, 2)
+        .toUpperCase();
+
+    const colors = ["#C0A450", "#4A90E2", "#50E3C2", "#B96BF5", "#F56262"];
+    const bg = colors[Math.floor(Math.random() * colors.length)];
+
+    return { initials, bg };
+    }
+
 }
