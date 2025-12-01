3eb98d527d903111a2cc4a58720c36383aaa801b
fco-barraza87 <advanceplus.coach@gmail.com>
2025-12-01 21:27:38 +0100

    Update courses.js

--- admin/courses.js
+++ admin/courses.js
@@ -1,6 +1,6 @@
 // /admin/courses.js
 import { supabase } from "/js/supabase.js";
-import { requireAdmin, setAdminHeader } from "/js/admin.js";
+import { requireAdmin, setAdminHeader } from "/admin/js/admin.js";
 
 let currentCourses = [];
 
