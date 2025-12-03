f027a61cc036e9c33e4f9d0f2530d1f451dfa1ba
fco-barraza87 <advanceplus.coach@gmail.com>
2025-12-03 12:49:41 +0100

    Update header.js

--- admin/components/header.js
+++ admin/components/header.js
@@ -106,7 +106,7 @@ function attachLogoutEvent() {
 
   btn.addEventListener("click", async () => {
     await supabase.auth.signOut();
-    window.location.href = "/login.html";
+    window.location.href = "/index.html";
   });
 }
 
