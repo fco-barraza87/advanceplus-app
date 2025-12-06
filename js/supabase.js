21f01c968592dcd37d21b71a9bec0beaea26a9d6
fco-barraza87 <advanceplus.coach@gmail.com>
2025-12-06 10:51:49 +0100

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,9 +1,8 @@
 
 
 // /js/supabase.js
-
 import { createClient } from 
-  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.51.0/dist/esm/supabase.mjs";
+  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.51.0/dist/esm/index.js";
 
 const SUPABASE_URL = "https://lmlfvbzukymtkcyfromr.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxtbGZ2Ynp1a3ltdGtjeWZyb21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5NjA3MzksImV4cCI6MjA3ODUzNjczOX0.VtetQ9jdaG6ZIDNGKsaTqxllET0nBZBrYDmZgfEdpLU";
